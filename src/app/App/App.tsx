d49253a46247ccba44c6768f35c166614ce56ff7
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2025-08-23 14:57:23 +0300

    feat: добавляет обработку данных useReducer

--- src/app/App/App.tsx
+++ src/app/App/App.tsx
@@ -1,21 +1,77 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import { MantineProvider } from "@mantine/core";
 
 import { launches } from "../../modules/services";
 
 import { TitleApp } from "../../modules/UI";
 
+interface LaunchInterface {
+  mission_name: string;
+  rocket?: {
+    rocket_name?: string;
+  };
+  links?: {
+    mission_patch?: string;
+    mission_patch_small?: string;
+  };
+  details?: string;
+}
+interface LaunchData {
+  mission_name?: string;
+  rocket_name?: string;
+  mission_patch?: string;
+  mission_patch_small?: string;
+  details?: string;
+}
+
+interface launchesDataInterface {
+  launchesData: LaunchData[];
+}
+
+const initialState = { launchesData: [] };
+
+function launchesReducer(
+  state: launchesDataInterface,
+  action: { type: string; payload: LaunchData[] }
+) {
+  switch (action.type) {
+    case "get_data":
+      return {
+        launchesData: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
 export const App = () => {
-  // const [launches2020, setLaunches2020] = useState([]);
+  const [state, dispatch] = useReducer(launchesReducer, initialState);
 
   useEffect(() => {
-    launches().then((data) => console.log(data));
+    launches().then((data: LaunchInterface[]) => {
+      const launchesEdit: LaunchData[] = data.map((launch) => {
+        return {
+          mission_name: launch.mission_name,
+          rocket_name: launch.rocket?.rocket_name,
+          mission_patch: launch.links?.mission_patch,
+          mission_patch_small: launch.links?.mission_patch_small,
+          details: launch.details,
+        };
+      });
+
+      dispatch({ type: "get_data", payload: launchesEdit });
+    });
   }, []);
 
   return (
     <MantineProvider>
       <TitleApp title="SpaceX Launches 2020" />
+      <div>
+        {state.launchesData.map((item) => {
+          return <li key={item.mission_name}>{item.mission_name}</li>;
+        })}
+      </div>
     </MantineProvider>
   );
 };
